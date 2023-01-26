05b1e1605c15901c0c1edec56021c474af93e394
dTap <imtapped@protonmail.com>
2023-01-26 22:33:40 +0700

    first commit

---
+++ src/Header.js
@@ -0,0 +1,14 @@
+// rafce is the shortcut to set up a new component
+import React from 'react'
+
+const Header = () => {
+  return (
+    <header>
+      <h1>
+        Grocery List
+      </h1>
+    </header>
+  )
+}
+
+export default Header
