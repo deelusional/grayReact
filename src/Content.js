2a81b940b0fdf63985f6a9a6d779eaaa523dbf81
dTap <imtapped@protonmail.com>
2023-01-30 22:06:35 +0700

    playing with useState and setCount

--- src/Content.js
+++ src/Content.js
@@ -1,18 +1,26 @@
 import React from 'react'
+import { useState } from 'react';
 
 const Content = () => {
+  // name (would be the 'getter') and setName (would be the 'setter')
+  const [name, setName] = useState('Moron');
+  const [count, setCount] = useState(0);
+
   const handleNameChange = () => {
     const names = ['Asshat', 'Dickhead', 'Fucktard'];
     const int = Math.floor(Math.random() * 3);
-    return names[int];
+    setName(names[int]);
+  }
+
+  const handleClick = () => { 
+    setCount(count + 1);
+    console.log(count);
   }
   
   const handleClick2 = (name) => { 
     console.log(`${name} was clicked!`);
   }
-  const handleClick = () => { 
-    console.log('You clicked it!');
-  }
+  
   const handleClick3 = (e) => { 
     // to get the event target
     console.log(e.target.innerText);
@@ -21,11 +29,16 @@ const Content = () => {
   return (
     <main>
       <p onDoubleClick={handleClick}>
-        Hello {handleNameChange()}!
+        {/* to use React useState (below) */}
+        Hello {name}!
+
+        {/* Below was using the vanilla js way */}
+        {/* Hello {handleNameChange()}! */}
       </p>
-      <button onClick={handleClick}>Click here</button>
+      <button onClick={handleNameChange}>Change Name</button>
+      <button onClick={handleClick}>Click It</button>
       {/* Anonymous function below */}
-      <button onClick={() =>  handleClick2('Asshat')  }>Click here</button>
+      {/* <button onClick={() =>  handleClick2('Asshat')  }>Click here</button> */}
       <button onClick={(e) =>  handleClick3(e)  }>Click here</button>
     </main>
   )
