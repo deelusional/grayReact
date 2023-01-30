40358e67142b4a4fa5bf186fc78a3f52941f011c
dTap <imtapped@protonmail.com>
2023-01-30 22:22:15 +0700

    added items, setItems

--- src/Content.js
+++ src/Content.js
@@ -3,34 +3,27 @@ import { useState } from 'react';
 
 const Content = () => {
   // name (would be the 'getter') and setName (would be the 'setter')
-  const [name, setName] = useState('Moron');
-  const [count, setCount] = useState(0);
-
-  const handleNameChange = () => {
-    const names = ['Asshat', 'Dickhead', 'Fucktard', 'Hastin Student', 'Idiot'];
-    const int = Math.floor(Math.random() * 3);
-    setName(names[int]);
-  }
-
-  const handleClick = () => { 
-    setCount(count + 1);
-    setCount(count + 1);
-    console.log(count);
-  }
+  const [items, setItems] = useState([
+    {
+      id: 1,
+      checked: false,
+      item: "One half pound bag of Cocoa Covered Almonds Unsalted"
+    }, 
+    {
+      id: 2,
+      checked: false,
+      item: "Item 2"
+    },
+    {
+      id: 3,
+      checked: false,
+      item: "Item 3"
+    }
+  ]);
   
-  const handleClick2 = () => { 
-    console.log(count);
-  }
-
   return (
     <main>
-      <p onDoubleClick={handleClick}>
-        {/* to use React useState (below) */}
-        Hello {name}!
-      </p>
-      <button onClick={handleNameChange}>Change Name</button>
-      <button onClick={handleClick}>Click It</button>
-      <button onClick={handleClick2}>Click here</button>
+      
     </main>
   )
 }
